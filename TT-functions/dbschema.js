4ac345c5913e72df57802be85f61f2ceed9280e1
Cory Bass <cory.bass.07@gmail.com>
2020-11-19 15:21:52 -0600

    new initial commit after hiding sensitive info and registering new API key

---
+++ TT-functions/dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  status: [
+    {
+      userHandle: "user",
+      body: "this is the status body",
+      createdAt: "2020-11-18T22:55:45.116Z",
+      likeCount: 5,
+      commentCount: 2,
+    },
+  ],
+};
---
+++ TT-functions/functions/index.js
@@ -0,0 +1,110 @@
+require("dotenv").config();
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+
+const app = require("express")();
+admin.initializeApp();
+
+const firebaseConfig = {
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  databaseURL: process.env.DATABASEURL,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGINGSENDERID,
+  appId: process.env.APPID,
+  measurementId: process.env.MEASUREMENTID,
+};
+
+const firebase = require("firebase");
+firebase.initializeApp(firebaseConfig);
+
+const db = admin.firestore();
+
+app.get("/all-status", (req, res) => {
+  db.collection("status")
+    .orderBy("createdAt", "desc")
+    .get()
+    .then((data) => {
+      let status = [];
+      data.forEach((doc) => {
+        status.push({
+          statusId: doc.id,
+          body: doc.data().body,
+          userHandle: doc.data().userHandle,
+          createdAt: doc.data().createdAt,
+        });
+      });
+      return res.json(status);
+    })
+    .catch((err) => console.error(err));
+});
+
+app.post("/post-status", (req, res) => {
+  const newStatus = {
+    body: req.body.body,
+    userHandle: req.body.userHandle,
+    createdAt: new Date().toISOString(),
+  };
+
+  db.collection("status")
+    .add(newStatus)
+    .then((doc) => {
+      res.json({ message: `document ${doc.id} created successfully` });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: "something went wrong" });
+      console.error(err);
+    });
+});
+
+// Signup
+app.post("/signup", (req, res) => {
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    handle: req.body.handle,
+  };
+
+  // TODO: validate data
+  let token, userId;
+  db.doc(`/users/${newUser.handle}`)
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        return res.status(400).json({ handle: "this handle is already taken" });
+      } else {
+        return firebase
+          .auth()
+          .createUserWithEmailAndPassword(newUser.email, newUser.password);
+      }
+    })
+    .then((data) => {
+      userId = data.user.uid;
+      return data.user.getIdToken();
+    })
+    .then((idToken) => {
+      token = idToken;
+      const userCredentials = {
+        handle: newUser.handle,
+        email: newUser.email,
+        createdAt: new Date().toISOString(),
+        userId,
+      };
+      db.doc(`/users/${newUser.handle}`).set(userCredentials);
+    })
+    .then(() => {
+      return res.status(201).json({ token });
+    })
+    .catch((err) => {
+      console.error(err);
+      if (err.code === "auth/email-already-in-use") {
+        return res.status(400).json({ email: "Email is already in use" });
+      } else {
+        return res.status(500).json({ error: err.code });
+      }
+    });
+});
+
+exports.api = functions.https.onRequest(app);
