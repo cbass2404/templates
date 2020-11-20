b57f91361728450f50f83b51fffff396359adecc
Cory Bass <cory.bass.07@gmail.com>
2020-11-20 09:09:43 -0600

    fixed bug where text files can be uploaded where there should be images

--- TT-functions/functions/handlers/users.js
+++ TT-functions/functions/handlers/users.js
@@ -113,9 +113,9 @@ exports.uploadImage = (req, res) => {
   let imageToBeUploaded = {};
 
   busboy.on("file", (fieldname, file, filename, encoding, mimetype) => {
-    console.log(fieldname);
-    console.log(filename);
-    console.log(mimetype);
+    if (mimetype !== "image/jpeg" && mimetype !== "image/png") {
+      return res.status(400).json({ error: "Wrong file type submitted" });
+    }
     const imageExtension = filename.split(".")[filename.split(".").length - 1];
     imageFileName = `${Math.round(
       Math.random() * 1000000000000
