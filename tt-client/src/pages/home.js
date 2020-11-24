1f9fc8a47491f4688475898d45161eac14d927fb
Cory Bass <cory.bass.07@gmail.com>
2020-11-24 00:56:52 -0600

    updated theme with typography

--- tt-client/src/pages/home.js
+++ tt-client/src/pages/home.js
@@ -1,17 +1,39 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 import Grid from "@material-ui/core/Grid";
 
 class Home extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      status: null,
+    };
   }
+
+  componentDidMount() {
+    axios
+      .get("/status")
+      .then((res) => {
+        this.setState({
+          status: res.data,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
   render() {
+    let recentStatusMarkup = this.state.status ? (
+      this.status.status.map((oneStatus) => <p>{oneStatus.body}</p>)
+    ) : (
+      <p>Loading...</p>
+    );
     return (
       <Grid container spacing={16}>
         <Grid item sm={8} xs={12}>
-          <p>Content...</p>
+          {recentStatusMarkup}
         </Grid>
         <Grid item sm={4} xs={12}>
           <p>Profile...</p>
