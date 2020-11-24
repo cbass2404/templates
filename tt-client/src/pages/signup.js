239414199f46edb959b1a42c7f96be7936787db7
Cory Bass <cory.bass.07@gmail.com>
2020-11-24 00:30:07 -0600

    client initial commit, basic setup ready

---
+++ tt-client/src/pages/signup.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Signup extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <h1>Signup page</h1>
+      </div>
+    );
+  }
+}
+
+export default Signup;
